3b7285eb150f5c26740eeddc86d7dc2d762c64e5
Luc Abayo <luc.bayo@gmail.com>
2020-01-23 16:23:43 +0200

    :sparkles: sparated widgets into different files (#4)

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,42 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questions = [
-    'What\'s your favorite color?',
-    'What\'s your favorite animal?'
-  ];
-  var questionIndex = 0;
-  void handleAnswer() {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _handleAnswer() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
   }
 
   @override
   Widget build(BuildContext contenxt) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('First App')),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
+            Question(questions[_questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: handleAnswer,
+              onPressed: _handleAnswer,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: handleAnswer,
+              onPressed: _handleAnswer,
             ),
             RaisedButton(
               child: Text('Answer 3'),
