045473bad5e6e95f1301cdf58d4e98a823b25032
Luc Abayo <luc.bayo@gmail.com>
2020-01-23 16:03:06 +0200

    :sparkles: handling button press and changing the state (#3)

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questions = [
+    'What\'s your favorite color?',
+    'What\'s your favorite animal?'
+  ];
+  var questionIndex = 0;
   void handleAnswer() {
-    print('Answer choosen!');
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
   }
 
   @override
   Widget build(BuildContext contenxt) {
-    var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?'
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('First App')),
         body: Column(
           children: [
-            Text('The question!'),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: handleAnswer,
