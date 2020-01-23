5e7a8a06f91aa07afeadd6741437e8e447b10f1b
Luc Abayo <luc.bayo@gmail.com>
2020-01-23 15:22:42 +0200

    Feature/button func binding (#2)
    * :sparkles: hooking function with a button onPressed

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,36 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void handleAnswer() {
+    print('Answer choosen!');
+  }
+
   @override
   Widget build(BuildContext contenxt) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('First App')),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: handleAnswer,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: handleAnswer,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => print('Answer 3 choosen'),
+            )
+          ],
+        ),
       ),
     );
   }
