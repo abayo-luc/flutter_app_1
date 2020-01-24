06e8958992080ef5af8aec06eddcfcffb9bd67fe
abayo-luc <luc.bayo@gmail.com>
2020-01-24 14:39:45 +0200

    :sparkles: score calculation

--- lib/widgets/results.dart
+++ lib/widgets/results.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
 class Results extends StatelessWidget {
+  final int score;
+  Results(this.score);
+  String get resultsPhase {
+    String resultsText;
+    if (score <= 8) {
+      resultsText = 'You are awosame and innocent!';
+    } else if (score <= 12) {
+      resultsText = 'You are likeable!';
+    } else if (score <= 16) {
+      resultsText = 'You are strange';
+    } else {
+      resultsText = 'You are so damn bad!';
+    }
+    return resultsText;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('You did it!'),
+    return Container(
+      margin: EdgeInsets.only(left: 15, right: 15),
+      child: Center(
+        child: Text(
+          resultsPhase,
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 }
