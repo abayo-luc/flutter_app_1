144fb48843244b0df6099853a810a99ba1513d79
Luc Abayo <luc.bayo@gmail.com>
2020-01-24 14:11:56 +0200

    :sparkles: splitting app into widgets (#8)

---
+++ lib/widgets/quiz.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
+
+class Quiz extends StatelessWidget {
+  final questions;
+  final int questionIndex;
+  final Function handleAnswer;
+  Quiz(
+      {@required this.questions,
+      @required this.questionIndex,
+      @required this.handleAnswer});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(questions[questionIndex]['question']),
+        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
+          return Answer(handleAnswer, answer);
+        }).toList(),
+      ],
+    );
+  }
+}
